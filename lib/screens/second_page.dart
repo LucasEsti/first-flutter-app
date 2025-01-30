3a7d598e766c7378ac0fd6e611dc4b0146e6c03f
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2025-01-30 16:19:07 +0300

    interaction avec les inputs des utilisateurs

--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _displayText = "";
+
+  void _updateText() {
+    setState(() {
+      _displayText = _controller.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Deuxième Page")),
-      body: Center(child: Text("Ceci est la deuxième page.")),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: "Entrez du texte"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _updateText,
+              child: Text("Afficher le texte"),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Texte saisi : $_displayText",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
